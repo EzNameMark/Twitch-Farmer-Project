e101fd07fff95cd4dec0f38c2acabee3add952f9
EzNameMark <64604845+EzNameMark@users.noreply.github.com>
2020-04-30 19:11:33 +0100

    Update Form1.Designer.cs

--- Twitch-Farmer-Project/Form1.Designer.cs
+++ Twitch-Farmer-Project/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.Start_Button = new System.Windows.Forms.Button();
             this.Stop_Button = new System.Windows.Forms.Button();
             this.Exit_Button = new System.Windows.Forms.Button();
+            this.Green_Box = new System.Windows.Forms.CheckBox();
+            this.Red_Box = new System.Windows.Forms.CheckBox();
+            this.none_box = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // Start_Button
@@ -63,18 +66,57 @@
             this.Exit_Button.UseVisualStyleBackColor = true;
             this.Exit_Button.Click += new System.EventHandler(this.Exit_Button_Click);
             // 
+            // Green_Box
+            // 
+            this.Green_Box.AutoSize = true;
+            this.Green_Box.Location = new System.Drawing.Point(93, 16);
+            this.Green_Box.Name = "Green_Box";
+            this.Green_Box.Size = new System.Drawing.Size(55, 17);
+            this.Green_Box.TabIndex = 3;
+            this.Green_Box.Text = "Green";
+            this.Green_Box.UseVisualStyleBackColor = true;
+            this.Green_Box.CheckedChanged += new System.EventHandler(this.Green_Box_CheckedChanged);
+            // 
+            // Red_Box
+            // 
+            this.Red_Box.AutoSize = true;
+            this.Red_Box.Location = new System.Drawing.Point(93, 45);
+            this.Red_Box.Name = "Red_Box";
+            this.Red_Box.Size = new System.Drawing.Size(46, 17);
+            this.Red_Box.TabIndex = 4;
+            this.Red_Box.Text = "Red";
+            this.Red_Box.UseVisualStyleBackColor = true;
+            this.Red_Box.CheckedChanged += new System.EventHandler(this.Red_Box_CheckedChanged);
+            // 
+            // none_box
+            // 
+            this.none_box.AutoSize = true;
+            this.none_box.Location = new System.Drawing.Point(93, 74);
+            this.none_box.Name = "none_box";
+            this.none_box.Size = new System.Drawing.Size(131, 17);
+            this.none_box.TabIndex = 5;
+            this.none_box.Text = "None (auto stops app)";
+            this.none_box.UseVisualStyleBackColor = true;
+            this.none_box.CheckedChanged += new System.EventHandler(this.none_box_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 166);
+            this.ClientSize = new System.Drawing.Size(311, 106);
+            this.Controls.Add(this.none_box);
+            this.Controls.Add(this.Red_Box);
+            this.Controls.Add(this.Green_Box);
             this.Controls.Add(this.Exit_Button);
             this.Controls.Add(this.Stop_Button);
             this.Controls.Add(this.Start_Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Twitch Farmer Project";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +125,9 @@
         private System.Windows.Forms.Button Start_Button;
         private System.Windows.Forms.Button Stop_Button;
         private System.Windows.Forms.Button Exit_Button;
+        private System.Windows.Forms.CheckBox Green_Box;
+        private System.Windows.Forms.CheckBox Red_Box;
+        private System.Windows.Forms.CheckBox none_box;
     }
 }
 

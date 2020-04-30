4bc545602446774e7397bee11cffd33e51ef7aff
EzNameMark <64604845+EzNameMark@users.noreply.github.com>
2020-04-30 19:08:18 +0100

    Update Form1.Designer.cs

--- Twitch-Farmer-Project/Form1.Designer.cs
+++ Twitch-Farmer-Project/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.Stop_Button = new System.Windows.Forms.Button();
+            this.Exit_Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.Location = new System.Drawing.Point(12, 12);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(75, 23);
+            this.Start_Button.TabIndex = 0;
+            this.Start_Button.Text = "Start";
+            this.Start_Button.UseVisualStyleBackColor = true;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // Stop_Button
+            // 
+            this.Stop_Button.Location = new System.Drawing.Point(12, 41);
+            this.Stop_Button.Name = "Stop_Button";
+            this.Stop_Button.Size = new System.Drawing.Size(75, 23);
+            this.Stop_Button.TabIndex = 1;
+            this.Stop_Button.Text = "Stop";
+            this.Stop_Button.UseVisualStyleBackColor = true;
+            this.Stop_Button.Click += new System.EventHandler(this.Stop_Button_Click);
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.Location = new System.Drawing.Point(12, 70);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.Size = new System.Drawing.Size(75, 23);
+            this.Exit_Button.TabIndex = 2;
+            this.Exit_Button.Text = "Exit";
+            this.Exit_Button.UseVisualStyleBackColor = true;
+            this.Exit_Button.Click += new System.EventHandler(this.Exit_Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(397, 166);
+            this.Controls.Add(this.Exit_Button);
+            this.Controls.Add(this.Stop_Button);
+            this.Controls.Add(this.Start_Button);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +79,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.Button Stop_Button;
+        private System.Windows.Forms.Button Exit_Button;
     }
 }
 
